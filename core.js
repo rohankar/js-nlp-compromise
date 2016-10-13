5914fbd8186424171629304f25555fab257758e8
Rohan Kar <rohankar@live.com>
2016-10-13 09:58:12 +0530

    work with syllables

---
+++ core.js
@@ -0,0 +1,7 @@
+var nlp = require('nlp_compromise');
+var nlpSyllables = require('nlp-syllables');
+
+nlp.plugin(nlpSyllables);
+var t2 = nlp.term('houston texas');
+t2.syllables();
+//[ [ 'hous', 'ton' ], [ 'tex', 'as' ] ]
